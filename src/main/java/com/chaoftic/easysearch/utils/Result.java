2d326c1a68fe59dd0e114e6db4fe75c4c741c344
王施超 <982030727@qq.com>
2018-10-27 23:37:48 +0800

    索引大概建立完了

--- src/main/java/com/chaoftic/easysearch/utils/Result.java
+++ src/main/java/com/chaoftic/easysearch/utils/Result.java
@@ -3,33 +3,34 @@ package com.chaoftic.easysearch.utils;
 @SuppressWarnings("unchecked")
 public class Result<T> {
 
-    enum ResultCode{
-        SUCCESS(200),//成功
-        FAIL(400),//失败
-        UNAUTHORIZED(403),//未认证（签名错误）
-        NOT_FOUND(404),//接口不存在
-        INTERNAL_SERVER_ERROR(500);//服务器内部错误
-        private final int code;
-        ResultCode(int code) {
-            this.code = code;
-        }
-    }
+    public static Integer SUCCESS = 200;
+    public static Integer FAIL = 400;//失败
+    public static Integer UNAUTHORIZED = 403;//未认证（签名错误）
+    public static Integer NOT_FOUND = 404;//接口不存在
+    public static Integer INTERNAL_SERVER_ERROR = 500;//服务器内部错误
+
 
     private int code;
     private String message;
     private T data;
 
-    public Result setCode(ResultCode code) {
-        this.code = code.code;
+    public static <T> Result success(T data) {
+        return new Result()
+                .setCode(SUCCESS)
+                .setMessage("success")
+                .setData(data);
+    }
+
+    public static <T> Result<T> success(T data, String message) {
+        return new Result()
+                .setCode(SUCCESS)
+                .setData(data)
+                .setMessage(message);
+    }
+
+    private Result setCode(Integer code) {
+        this.code = code;
         return this;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     public Result setMessage(String message) {
@@ -37,26 +38,8 @@ public class Result<T> {
         return this;
     }
 
-    public T getData() {
-        return data;
-    }
-
     public Result setData(T data) {
         this.data = data;
         return this;
-    }
-
-    public static<T> Result success(T data) {
-        return new Result()
-                .setCode(ResultCode.SUCCESS)
-                .setMessage("success")
-                .setData(data);
-    }
-
-    public static<T> Result<T> success(T data, String message){
-        return new Result()
-                .setCode(ResultCode.SUCCESS)
-                .setData(data)
-                .setMessage(message);
     }
 }
